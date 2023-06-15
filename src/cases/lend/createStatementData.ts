cdfe8ab41527765620c9849ffa8cfcd4f7ac3b5e
Dr-ZHUIM <372919916@qq.com>
2023-06-15 14:51:58 +0800

    1. create class to manage object's perf play amount and volumnCredits 2. transform calcAmount and calcCredit to class 3. create a factory function to create child object

--- src/cases/lend/createStatementData.ts
+++ src/cases/lend/createStatementData.ts
@@ -1,3 +1,4 @@
+import { PerformanceCalculator } from "./class";
 export default function createStatementData(invoice: Invoice, plays: Record<string, Play>){
   const statementData:StatementData = {
     customer: invoice.customer,
@@ -8,11 +9,13 @@ export default function createStatementData(invoice: Invoice, plays: Record<stri
   statementData.totalCredits = calcTotalCredits(statementData);
   statementData.totalAmount = calcTotalAmount(statementData);
   return statementData;
+
   function enrichPerformance(performance:CPerformance){
+    const calculator = createPerformanceCalculator(performance,getPlay(performance))
     const result = Object.assign({},performance) as CPerformanceEnriched;
-    result.play = getPlay(result);
-    result.amount = calcAmount(result);
-    result.volumnCredits = calcCredit(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumnCredits = calculator.volumnCredits;
     return result;
   }
 
@@ -20,37 +23,9 @@ export default function createStatementData(invoice: Invoice, plays: Record<stri
     return plays[perf.playID]
   }
 
-  function calcAmount (perf:CPerformanceEnriched){
-    let result = 0;
-      switch (perf.play.type) {
-        case 'tragedy': {
-          result = 40000;
-          if (perf.audience > 30) {
-            result += 1000 * (perf.audience - 30);
-          }
-          break;
-        }
-        case 'comedy': {
-          result = 30000;
-          if (perf.audience > 20) {
-            result += 10000 + 500 * (perf.audience - 20);
-          }
-          result += 300 * perf.audience;
-          break;
-        }
-        default: {
-          throw new Error(`unknown type: ${perf.play.type}`);
-        }
-      }
-      return result;
-  }         
-  
-  function calcCredit(perf:CPerformanceEnriched){
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if (perf.play.type === 'comedy') result += Math.floor(perf.audience / 5);
-    return result;
-  }
+  // function getAmount(perf:CPerformanceEnriched){
+  //   return new PerformanceCalculator(perf,getPlay(perf)).amount;
+  // }
 
   function calcTotalCredits(data:StatementData){
     return data.performances.reduce((total,p)=>total + p.volumnCredits,0)
@@ -59,4 +34,9 @@ export default function createStatementData(invoice: Invoice, plays: Record<stri
   function calcTotalAmount(data:StatementData){
     return data.performances.reduce((total,p)=>total + p.amount,0)
   }
+}
+
+
+function createPerformanceCalculator(perf:CPerformance,play:Play){
+  return new PerformanceCalculator(perf,play);
 }
